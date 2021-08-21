4e82556308e993c70cfd43614821156286719570
ReyCodeSSH <72778500+RmXF@users.noreply.github.com>
2021-08-21 00:30:08 -0300

    Update ADMbot.sh

--- ADMbot.sh
+++ ADMbot.sh
@@ -464,13 +464,13 @@ SEC=$(($TIMEON-$MIN*60))
 HOR=$(($MIN/60))
 MIN=$(($MIN-$HOR*60))
 HOUR="${HOR}h:${MIN}m:${SEC}s"
-[[ -z $(cat ${USRdatabase}|grep -w "${user}") ]] && MAXUSER="?" || MAXUSER="$(cat ${USRdatabase}|grep -w "${user}"|cut -d'|' -f4)"
+[[ -z $(cat ${USRdatabase}|grep -w "${user}") ]] && MAXUSER="-" || MAXUSER="$(cat ${USRdatabase}|grep -w "${user}"|cut -d'|' -f4)"
 [[ $(echo $PID|bc) -gt 0 ]] && user="$user           [${verde}ONLINE${cierre}]" || user="$user           [${rojo}OFLINE${cierre}]"
 TOTALPID="$(echo $PID|bc)/$MAXUSER"
  while [[ ${#user} -lt 45 ]]; do
  user=$user" "
  done
- while [[ ${#TOTALPID} -lt 13 ]]; do
+ while [[ ${#TOTALPID} -lt 18 ]]; do
  TOTALPID=$TOTALPID" "
  done
  while [[ ${#HOUR} -lt 8 ]]; do
