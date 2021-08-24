0e38b9d2cc1ae4a277ed35730c4e8fa3f7b57230
ReyCodeSSH <72778500+RmXF@users.noreply.github.com>
2021-08-23 21:12:03 -0300

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -580,7 +580,7 @@ echo -e "${azul}Usuarios Actualmente activos en el servidor${cierre}"
 echo -e "$bar1"
 i=0
 for us in $(echo ${usuarios_ativos[@]}); do
-echo -ne "[$i] ->" && echo -e "\033[1;33m ${us}"
+echo -ne "[$i] ==>" && echo -e "\033[1;33m ${us}"
 let i++
 done
 echo -e "$bar1"
@@ -656,6 +656,52 @@ done
 echo -e "$bar1"
 edit_user_fun "${useredit}" "${senhauser}" "${diasuser}" "${limiteuser}" && echo -e "${verde}Usuario Modificado Con Exito${cierre}" || echo -e "${rojo}Error, Usuario no modificado${cierre}"
 echo -e "$bar1"
+read -p  " ➢ Presione enter para volver " 
+rm -rf /etc/usr/bin/usercode; usercode
+}
+
+eliminar_usuario () {
+usuarios_ativos=($(mostrar_usuarios))
+if [[ -z ${usuarios_ativos[@]} ]]; then
+echo -e "${rojo}Ningun usuario registrado${cierre}"
+echo -e "$bar1"
+return 1
+else
+echo -e "${amarillo}Usuarios actualmente activos en el servidor${cierre}"
+echo -e "$bar1"
+i=0
+for us in $(echo ${usuarios_ativos[@]}); do
+echo -ne "[$i] ==>" && echo -e "\033[1;33m ${us}"
+let i++
+done
+echo -e "$bar1"
+fi
+echo -e "${blanco}Escriba o seleccione un usuario${cierre}"
+echo -e "$bar1"
+unset selection
+while [[ -z ${selection} ]]; do
+echo -ne "\033[1;37mSeleccione una opcion: " && read selection
+tput cuu1 && tput dl1
+done
+if [[ ! $(echo "${selection}" | egrep '[^0-9]') ]]; then
+usuario_del="${usuarios_ativos[$selection]}"
+else
+usuario_del="$selection"
+fi
+[[ -z $usuario_del ]] && {
+     echo -e "${rojo}Error, Usuario Invalido${cierre}"
+     echo -e "$bar1"
+     return 1
+     }
+[[ ! $(echo ${usuarios_ativos[@]}|grep -w "$usuario_del") ]] && {
+     echo -e "${rojo}Error, Usuario Invalido${cierre}"
+     echo -e "$bar1"
+     return 1
+     }
+echo -ne "${blanco}Usuario Selecionado: " && echo -ne "$usuario_del"
+rm_user "$usuario_del" && echo -e "${verde} [ELIMINADO]${blanco}" || echo -e "${rojo} [NO ELIMINADO]${cierre}"
+sed -i "/$usuario_del/d" $USRdatabase
+echo -e "$bar4"
 read -p  " ➢ Presione enter para volver " 
 rm -rf /etc/usr/bin/usercode; usercode
 }
@@ -784,8 +830,9 @@ ${blanco}[${cierre}${rojo}09${cierre}${blanco}]${cierre} ${rojo}>${cierre} ${bla
 ${blanco}[${cierre}${rojo}10${cierre}${blanco}]${cierre} ${rojo}>${cierre} ${blanco}Detalles de la  ${amarillo}========${cierre}${rojo}>${cierre} ${azul}maquina${cierre}
 ${blanco}[${cierre}${rojo}11${cierre}${blanco}]${cierre} ${rojo}>${cierre} ${blanco}Crear copia de  ${amarillo}========${cierre}${rojo}>${cierre} ${verde}usuarios${cierre}
 ${blanco}[${cierre}${rojo}12${cierre}${blanco}]${cierre} ${rojo}>${cierre} ${blanco}Instalar metodo  ${amarillo}=======${cierre}${rojo}>${cierre} ${melon}SSL+PYT.D${cierre}
-${blanco}[${cierre}${rojo}13${cierre}${blanco}]${cierre} ${rojo}>${cierre} ${blanco}Añadir banner  ${amarillo}=========${cierre}${rojo}>${cierre} ${melon}ssh${cierre} 
-${blanco}[${cierre}${rojo}14${cierre}${blanco}]${cierre} ${rojo}>${cierre} ${rojo}Eliminar todos los ${amarillo}=======${cierre}${rojo}>${cierre} ${rojo}usuarios{cierre} 
+${blanco}[${cierre}${rojo}13${cierre}${blanco}]${cierre} ${rojo}>${cierre} ${blanco}Añadir banner  ${amarillo}=========${cierre}${rojo}>${cierre} ${amarillo}ssh${cierre} 
+${blanco}[${cierre}${rojo}14${cierre}${blanco}]${cierre} ${rojo}>${cierre} ${rojo}Eliminar todos los =======>Usuarios{cierre} 
+${bar4}"
 ${blanco}[${cierre}${rojo}0${cierre}${blanco}]${cierre} ${rojo}>>>${cierre} ${resaltadorojo} SALIR ${cierre1}
 ${bar4}"
 read -p "$(echo -e "${blanco}seleccione [0-13]:${cierre}")" selection
