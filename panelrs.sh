55d8068963720ad941fd6e0d82dbb42015fc0f3c
ReyCodeSSH <72778500+RmXF@users.noreply.github.com>
2021-08-30 21:45:16 -0300

    Update panelrs.sh

--- panelrs.sh
+++ panelrs.sh
@@ -135,73 +135,7 @@ baner () {
 read -p " ➢ Presione enter para volver "
 rm -rf /etc/usr/bin/usercode; usercode
 }
-
-
-fun_udp1 () {
-    [[ -e "/bin/badvpn-udpgw" ]] && {
-    clear
-    echo -e "\033[1;32mINICIANDO O BADVPN... \033[0m\n"
-    fun_udpon () {
-        screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
-        [[ $(grep -wc "udpvpn" /etc/autostart) = '0' ]] && {
-		    echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
-		} || {
-		    sed -i '/udpvpn/d' /etc/autostart
-		    echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
-		}
-        sleep 1
-    }
-    fun_bar 'fun_udpon'
-    echo -e "\n  \033[1;32mBADVPN ATIVO !\033[0m"
-    sleep 3
-    usercode
-    } || {
-        clear
-        echo -e "\033[1;32mINSTALANDO O BADVPN !\033[0m\n"
-	    inst_udp () {
-	        cd $HOME
-            wget https://www.dropbox.com/preview/badvpn-udpgw -o /dev/null
-            mv -f $HOME/badvpn-udpgw /bin/badvpn-udpgw
-            chmod 777 /bin/badvpn-udpgw
-	   }
-	   fun_bar 'inst_udp'
-	   echo -e "\n\033[1;32mINICIANDO O BADVPN... \033[0m\n"
-       fun_udpon2 () {
-           screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
-           [[ $(grep -wc "udpvpn" /etc/autostart) = '0' ]] && {
-		       echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
-		   } || {
-		       sed -i '/udpvpn/d' /etc/autostart
-		       echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
-		   }
-           sleep 1
-       }
-       fun_bar 'fun_udpon2'
-       echo -e "\n\033[1;32mBADVPN ATIVO !\033[0m"
-       sleep 3
-       usercode
-    }
-}
-
-fun_udp2 () {
-    clear
-    echo -e "\n\033[1;32mPARANDO O BADVPN...\033[0m\n"
-    fun_stopbad () {
-        sleep 1
-        screen -r -S "udpvpn" -X quit
-        screen -wipe 1>/dev/null 2>/dev/null
-        [[ $(grep -wc "udpvpn" /etc/autostart) != '0' ]] && {
-		    sed -i '/udpvpn/d' /etc/autostart
-		}
-        sleep 1
-    }
-    fun_bar 'fun_stopbad'
-    echo -e "\n  \033[1;31mBADVPN PARADO !\033[0m"
-    sleep 3
-    menu
-}
-[[ $(ps x | grep "udpvpn"|grep -v grep |wc -l) = '0' ]] &&  fun_udp1 || fun_udp2
-
+	
 
 
 # admin udp
