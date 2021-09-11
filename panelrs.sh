913e10cdd5865b5b215e52426e81796514be3d31
ReyCodeSSH <72778500+RmXF@users.noreply.github.com>
2021-09-11 19:11:35 -0300

    Update panelrs.sh

--- panelrs.sh
+++ panelrs.sh
@@ -5,6 +5,41 @@ Desarrollador @ReyRs_ViPro
 ## SCRIPT RS CREACIONES POWERED BY FABIAN
 ## GENERENCIADOR DE USUARIOS
 
+
+TITLE=' =•= REYCODESSH =•= ';
+GRN1='\e[32m [\e[0m1\e[32m]\e[0m';
+GRN2='\e[32m [\e[0m2\e[32m]\e[0m';
+GRN3='\e[32m [\e[0m3\e[32m]\e[0m';
+GRN4='\e[32m [\e[0m4\e[32m]\e[0m';
+GRN5='\e[32m [\e[0m5\e[32m]\e[0m';
+GRN6='\e[32m [\e[0m6\e[32m]\e[0m';
+GRN7='\e[32m [\e[0m7\e[32m]\e[0m';
+GRN8='\e[32m [\e[0m8\e[32m]\e[0m';
+GRN9='\e[32m [\e[0m9\e[32m]\e[0m';
+GRN10='\e[32m [\e[0m10\e[32m]\e[0m';
+GRN11='\e[32m [\e[0m11\e[32m]\e[0m';
+GRN12='\e[32m [\e[0m12\e[32m]\e[0m';
+GRN13='\e[32m [\e[0m13\e[32m]\e[0m';
+GRN14='\e[32m [\e[0m14\e[32m]\e[0m';
+GRN15='\e[32m [\e[0m15\e[32m]\e[0m';
+GRN16='\e[32m [\e[0m16\e[32m]\e[0m';
+GRN17='\e[32m [\e[0m17\e[32m]\e[0m';
+GRN18='\e[32m [\e[0m18\e[32m]\e[0m';
+GRN19='\e[32m [\e[0m19\e[32m]\e[0m';
+GRN20='\e[32m [\e[0m20\e[32m]\e[0m';
+GRN21='\e[32m [\e[0m21\e[32m]\e[0m';
+GRN22='\e[32m [\e[0m22\e[32m]\e[0m';
+GRN23='\e[32m [\e[0m23\e[32m]\e[0m';
+GRN24='\e[32m [\e[0m24\e[32m]\e[0m';
+GRN25='\e[32m [\e[0m25\e[32m]\e[0m';
+GRN26='\e[32m [\e[0m26\e[32m]\e[0m';
+GRN27='\e[32m [\e[0m27\e[32m]\e[0m';
+GRN28='\e[32m [\e[0m28\e[32m]\e[0m';
+GRN29='\e[32m [\e[0m29\e[32m]\e[0m';
+GRN30='\e[32m [\e[0m30\e[32m]\e[0m';
+GRN31='\e[32m [\e[0m31\e[32m]\e[0m';
+GRN32='\e[32m [\e[0m32\e[32m]\e[0m';
+
 ## VARIABLES DE COLORES
 blanco='\033[38;5;231m'
 amarillo='\033[38;5;228m'
@@ -787,7 +822,7 @@ clear
 on="\033[1;92m[ON]" && off="\033[1;31m[OFF]"
 [[ $(ps x | grep badvpn | grep -v grep | awk '{print $1}') ]] && badvpn=$on || badvpn=$off
 echo -e "\033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[41;1;37m                              ⇱  REYCODESSH  ⇲                      \e[0m\e[7;32m [ V2.6 ] \e[0m"
+echo -e "\e[41;1;37m                              ⇱  $TITLE  ⇲                      \e[0m\e[7;32m [ V2.6 ] \e[0m"
 echo -e "\033[1;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "${verde}CUENTAS SSH - DROPLET - SCRIPT - APK MOD - DISEÑO WEB ( ${rojo}dev:${cierre} ${melon}@ReyRs_ViPro${cierre} )
 ${bar4}
